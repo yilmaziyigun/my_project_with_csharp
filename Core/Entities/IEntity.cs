b9409742a4129c6adaf5177581dc17af149fb8c5
Yilmaz <yilmaziyigun1@gmail.com>
2021-02-22 23:42:55 +0300

    using core

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities.Abstract
 {
     //IEntity imlement eden class bir veritabanı tablosudur.
     public interface IEntity
